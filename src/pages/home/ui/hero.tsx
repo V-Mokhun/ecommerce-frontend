f17de62054c3699f9dbf995a1d4467333ee59554
V-Mokhun <mohunmax1408@gmail.com>
2023-10-05 22:20:13 +0200

    Added hero section

--- src/pages/home/ui/hero.tsx
+++ src/pages/home/ui/hero.tsx
@@ -8,21 +8,32 @@ export const Hero = ({}: HeroProps) => {
   return (
     <section>
       <Container>
-        <div className="flex justify-between">
-          <div className="flex flex-col h-full">
-            <h1 className="font-semibold text-2xl md:text-4xl lg:text-6xl text-primary-700 mb-2 md:mb-5 lg:mb-8">
+        <div className="flex justify-between mt-6 md:mt-0">
+          <div className="flex-auto mt-4 md:mt-10">
+            <h1 className="font-medium md:font-semibold text-2xl sm:text-4xl md:text-5xl lg:text-6xl text-primary-700 mb-2 md:mb-5 lg:mb-8">
               Ecommerce
             </h1>
-            <p className="text-primary-700 font-medium text-xs xs:text-base md:text-xl lg:text-3xl">
+            <p className="text-primary-700 font-medium text-xs xs:text-lg sm:text-xl md:text-2xl lg:text-3xl">
               "Join the{" "}
               <span className="text-secondary">digital revolution</span>"
             </p>
             <NavLink
               to={PRODUCTS_ROUTE}
-              className={buttonVariants({ variant: "secondary" })}
+              className={buttonVariants({
+                variant: "secondary",
+                className:
+                  "md:w-full max-w-xs mt-14 xs:mt-16 sm:mt-20 md:mt-24 lg:mt-28",
+              })}
             >
               Explore More
             </NavLink>
+          </div>
+          <div className="-ml-10 flex-[0_1_60%]">
+            <img
+              className="object-cover max-w-full h-auto"
+              src="/images/hero.png"
+              alt="Hero"
+            />
           </div>
         </div>
       </Container>
