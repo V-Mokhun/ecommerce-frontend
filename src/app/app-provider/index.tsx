7ac47d01e169d6f64cdfcd506850f60f97b681e6
V-Mokhun <mohunmax1408@gmail.com>
2023-10-19 10:00:17 +0200

    Add redux-persist

--- src/app/app-provider/index.tsx
+++ src/app/app-provider/index.tsx
@@ -1,12 +1,15 @@
 import { apolloClient } from "@/shared/lib/apollo";
-import { store } from "@/store";
+import { persistor, store } from "@/store";
 import { ApolloProvider } from "@apollo/client";
 import { Provider as ReduxProvider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 export const AppProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <ReduxProvider store={store}>
-      <ApolloProvider client={apolloClient}>{children}</ApolloProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ApolloProvider client={apolloClient}>{children}</ApolloProvider>
+      </PersistGate>
     </ReduxProvider>
   );
 };
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,42 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { productsSlice } from "./products";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { cartSlice } from "./cart/slice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "e-commerce-react",
+  storage,
+};
+
+const cartPersistedReducer = persistReducer(persistConfig, cartSlice.reducer);
 
 export const store = configureStore({
   reducer: {
     [productsSlice.name]: productsSlice.reducer,
+    [cartSlice.name]: cartPersistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
-export const useAppDispatch: () => AppDispatch = useDispatch
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
